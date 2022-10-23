f322e5b330b43a33e17d3c4453f88474a24475ff
chenzongwei <63836858+chen-001@users.noreply.github.com>
2022-10-24 00:03:35 +0800

    Initial commit

---
+++ sengbao_mle/__init__.py
@@ -0,0 +1,9 @@
+__version__='0.0.1'
+
+__all__=['cores','distributions','model','tests','variable']
+
+from sengbao_mle.cores.minuit import *
+from sengbao_mle.cores.util import *
+from sengbao_mle.distributions import *
+from sengbao_mle.model.model import *
+from sengbao_mle.variable.variable import *
